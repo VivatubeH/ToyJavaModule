98386e082ed8a7e538a239b62201959713726950
001516291 <001516291@kyonggi.ac.kr>
2025-03-07 16:56:08 +0900

    feat(1) : 달력 클래스 생성

---
+++ src/main/CalendarCreatorMain.java
@@ -0,0 +1,10 @@
+package main;
+
+import util.CalendarCreator;
+
+public class CalendarCreatorMain {
+    public static void main(String[] args) {
+        CalendarCreator creator = new CalendarCreator();
+        creator.printCalendar();
+    }
+}
